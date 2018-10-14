3c313b4f1d1f5c2866b7bc180172f95238ce243c
Scott Brown <accounts@thinkthirdrail.co.uk>
2018-10-14 09:37:06 +0100

    More changes

--- wp-content/themes/2posh2pitch/front-page.php
+++ wp-content/themes/2posh2pitch/front-page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 
 <section class="banner">
-    <div class="overlay"></div>
     <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/eWdj5_v5YAQ?rel=0&amp;controls=0&amp;loop=1&playlist=eWdj5_v5YAQ;showinfo=0;autoplay=1;start=29;end=31;" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
     <div class="cta">
         <i class="fas fa-times"></i>
@@ -10,16 +9,26 @@
             <h2><span>Select a pitch</span></h2>
         </div>
         <div class="buttons">
-            <div class="button-blue"><p>Burnham Breck</p></div>
-            <div class="button-white"><p>Whitehall Farm</p></div>
+            <div class="button-blue"><p>Burnham Breck</p><i class="fas fa-arrow-right"></i></div>
+            <div class="button-white"><p>Whitehall Farm</p><i class="fas fa-arrow-right"></i></div>
+        </div>
+        <div class="social">
+            <a href="#" id="fb"><p>Facebook</p></a> <span class="seperate">•</span>
+            <a href="#" id="twitter"><p>Twitter</p></a> <span class="seperate">•</span>
+            <a href="#" id="instagram"><p>Instagram</p></a> <span class="seperate">•</span>
+            <a href="#" id="youtube"><p>Youtube</p></a>
         </div>
     </div>
+    <div class="overlay"></div>
     <p class="tag">A BEAUTUFUL<br> ESCAPE</p>
 </section>
 <section class="pre-blog">
-    <main class="wrapper">
-        <div class="blue-block">
-            <div class="offset-box">
+
+        <div class="blue-block"></div>
+        <div class="grey-block"></div>
+
+        <div class="left-block">
+            <main class="wrapper">
                 <div class="head-text">
                     <h1>Luxury</h1>
                     <h2><span>Bell tent hire</span></h2>
@@ -34,12 +43,44 @@
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra, dolor et lacinia ornare, nibh ante mollis enim, ac convallis libero sem id massa. Maecenas ligula purus, vulputate at massa quis, consequat pharetra urna. Duis eget orci euismod, molestie lorem vel, venenatis magna.
                 </p>
+                <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/sig.svg" />
+            </main>
+        </div>
+
+        <div class="right-block">
+            <main class="wrapper">
+                <div class="offset-box">
+                    <p class="italic">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet euismod purus.
+                    </p>
+                </div>
+            </main>
+        </div>
+
+</section>
+<section class="home-blog">
+    <div class="blue-block"></div>
+    <div class="clear-block"></div>
+    <div class="blog-box">
+        <div class="blog-image">
+            <div class="buttons">
+                <div class="button-white"><p>Read More on this post</p><i class="fas fa-arrow-right"></i></div>
+            </div>
+            <div class="overlay"></div>
+        </div>
+        <div class="blog-content">
+            <i class="fas fa-times"></i>
+            <div class="head-text">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <div class="divider"></div>
+                <h2>12th January 2018</h2>
             </div>
         </div>
-        <div class="grey-block"></div>
-    </main>
+    </div>
 </section>
-<section class="home-blog"></section>
-<section class="post-blog"></section>
+<section class="post-blog">
+    <div class="left-block"></div>
+    <div class="right-block"></div>
+</section>
 
 <?php get_footer(); ?>
